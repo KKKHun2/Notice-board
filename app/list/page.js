0a832733b0af734973ded25b50d11e248c65fb25
KKKHun2 <1224kim2son@naver.com>
2023-05-18 00:03:26 +0900

    ✨게시글 삭제기능 추가

--- app/list/page.js
+++ app/list/page.js
@@ -1,7 +1,9 @@
-
-
 import { connectDB } from "@/util/database";
 import ListItem from "./Listitem";
+
+export const dynamic = 'force-dynamic' //매번 서버로 요청
+// export const static = 'force-static' 회원들이 많이 들어와도 저장된html만보여줌 
+
 export default async function List() {
 
  
--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 import { connectDB } from "@/util/database"
-import { MongoClient } from "mongodb";
 
 
 export default async function Home() {
@@ -7,7 +6,10 @@ export default async function Home() {
   const client = await connectDB;
   const db = client.db("forum")
   let result = await db.collection('post').find().toArray();
-
+  // await fetch('/URL', {cache:'force-cache'})  결과 몰래 저장하고 사용
+  // await fetch('/URL', {cache:'no-store'})  매번 서버로 요청해서 새거 가져온다.
+  // await fetch('/URL', {next:{revalidate:60}})  60초마다 캐싱된 데이터 갱신
+  
   return (
     <div>{result[0].title}</div>
   )
